34bafd69f1653c4bf40f078df51b26ad337cde57
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2026-02-13 14:21:15 -0500

    HGI-9701: Remove unused conditional (#19)

--- target_mailchimp_v2/target.py
+++ target_mailchimp_v2/target.py
@@ -33,7 +33,7 @@ class TargetMailChimpV2(TargetHotglue):
             "contacts",
             "customer",
             "contact",
-        ] and not self.config.get("use_fallback_sink")) or stream_name.lower() == "list_members" and not self.config.get("process_batch_contacts", True):
+        ] and not self.config.get("use_fallback_sink")):
             return MailChimpV2Sink
         elif stream_name.lower() == "custom_fields":
             return CustomFieldsSink
